f1d0c0db96672b73791dda0df8ceb047c1a099cd
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-07-24 12:06:25 +0200

    form data validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  /**
+   * Challenge: Connect the form to local state
+   *
+   * 1. Create a state object to store the 4 values we need to save.
+   * 2. Create a single handleChange function that can
+   *    manage the state of all the inputs and set it up
+   *    correctly
+   * 3. When the user clicks "Sign up", check if the
+   *    password & confirmation match each other. If
+   *    so, log "Successfully signed up" to the console.
+   *    If not, log "passwords to not match" to the console.
+   * 4. Also when submitting the form, if the person checked
+   *    the "newsletter" checkbox, log "Thanks for signing
+   *    up for our newsletter!" to the console.
+   */
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    confirm_password: "",
+    checkbox: true,
+  });
+  function handleChange(event) {
+    setFormData(prev=>{
+      const {name,value}=event.target
+      return({
+        ...prev,
+        [name]:value,
+        checkbox:!formData.checkbox
+      })
+    })
+  }
+  function submit(e){
+e.preventDefault()
+console.log(formData)
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-purple-500">
+      <div className="w-[30%] mx-auto rounded-md bg-white">
+        <form onSubmit={submit} className="w-[60%] mx-auto grid">
+          <input
+            className="border rounded-sm my-2"
+            type="text"
+            value={FormData.email}
+            name="email"
+            onChange={handleChange}
+          />
+          <input
+            className="border rounded-sm my-2"
+            type="text"
+            value={FormData.password}
+            name="password"
+            onChange={handleChange}
+          />
+          <input
+            className="border rounded-sm my-2"
+            type="text"
+          value={FormData.confirm_password}
+            name="confirm_password"
+            onChange={handleChange}
+          />
+          <div className="flex gap-4">
+            <input
+              type="checkbox"
+              id="checkbox"
+              name="checkbox"
+              value={FormData.checkbox}
+              onChange={handleChange}
+            />
+            <label htmlFor="checkbox">I want to join the newsletter</label>
+          </div>
+
+          <button  className="bg-purple-500 text-white w-[27%] mx-auto rounded-sm ">
+            Sign up
+          </button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
